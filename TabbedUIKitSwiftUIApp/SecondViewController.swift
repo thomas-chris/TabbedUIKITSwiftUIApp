df1b495cba8485ffff943cd7c69176733cb7f591
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-07-04 17:12:59 +0100

    Initial Commit

---
+++ TabbedUIKitSwiftUIApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabbedUIKitSwiftUIApp
+//
+//  Created by Chris Thomas on 04/07/2020.
+//  Copyright © 2020 Chris Thomas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
